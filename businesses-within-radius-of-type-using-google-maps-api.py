46845e6590f6db3aae20789e8a796617e152bc20
J. Chris Wingeier II <52218669+jcwii@users.noreply.github.com>
2022-03-17 19:45:30 -0400

    Create businesses-within-radius-of-type-using-google-maps-api.py

---
+++ businesses-within-radius-of-type-using-google-maps-api.py
@@ -0,0 +1,36 @@
+import requests,json
+
+key=''
+radius='50000'
+type='book_store'
+keyword=''
+location='39.16285,-84.81207'
+
+r=requests.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+location+'&radius='+radius+'&type='+type+'&key='+key+')
+
+with open('c:\\users\\user\\downloads\\nearbySearch.json','w') as f:
+    f.write(json.dumps(r.json(), indent=4, sort_keys=True))
+
+#########################################
+ar=[]
+with open('c:\\users\\user\\downloads\\nearbySearch.json') as f:
+    data=json.load(f)
+    for i in data['results']:
+        #sort for names like dominos
+        ar.append(i['place_id'])
+#########################################
+
+with open('c:\\users\\user\\downloads\\details.json','w') as f:
+d={}
+for placeid in ar:
+    r=requests.get('https://maps.googleapis.com/maps/api/place/details/json?placeid='+placeid+'&key='+key)
+    data=r.json()
+    k=data['result']['name']
+    d[k]=data
+
+with open('c:\\users\\user\\downloads\\details.json') as f:
+    data=json.load(f)
+    ar=[]
+    for k,v in data['result'].items():
+        ar.append(v)
+    print(ar)
